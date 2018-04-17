b8efd4ddd4a3610d4201c8e47594e84d9a268dd9
JJ Herasymchuk <evhenii.herasymchuk@gmail.com>
2018-04-17 20:05:36 +0200

    Eject the project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      title: 'Hey'
-    };
-
-    console.log('one line ');
-
-    console.log();
-  }
-
-  componentWillMount() {
-    console.log('we are in will mount hook');
-  }
-
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="App"> </div>;
   }
 }
 
---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const layout = props => [
+  <div key="1">Toolbar, SideDrawer, Backdrop</div>,
+  <main key="2" />
+];
+
+export default layout;
