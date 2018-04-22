0f1a267dd79cd0efb1b8ccfbeec5d4b5dcd7fe9a
JJ Herasymchuk <evhenii.herasymchuk@gmail.com>
2018-04-22 15:13:47 +0200

    Control the rendering of modal window with shouldComponentUpdate hook

--- src/components/BurgerOrderModal/BurgerOrderModal.js
+++ src/components/BurgerOrderModal/BurgerOrderModal.js
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import OrderModal from '../OrderModal/OrderModal';
 
-const BurgerOrderModal = props => {
-  const ingredientsList = Object.keys(props.ingredients).map(key => {
-    return (
-      <span key={key}>
-        {' '}
-        {key} : {props.ingredients[key]}{' '}
-      </span>
-    );
-  });
-  return (
-    <OrderModal
-      title={'Finish your order'}
-      visible={props.modalShown}
-      onCancel={props.closeModalHandler}
-      onSuccess={props.successModalHandler}
-      cancelButtonText={'Cancel'}
-      successButtonText={'Order'}
-    >
-      <div>Total price: ${props.totalPrice}</div>
-      {ingredientsList}
-    </OrderModal>
-  );
-};
+class BurgerOrderModal extends Component {
+  static propTypes = {
+    modalShown: PropTypes.bool.isRequired,
+    closeModalHandler: PropTypes.func.isRequired,
+    successModalHandler: PropTypes.func.isRequired,
+    ingredients: PropTypes.object.isRequired,
+    totalPrice: PropTypes.number.isRequired
+  };
 
-BurgerOrderModal.propTypes = {
-  modalShown: PropTypes.bool,
-  closeModalHandler: PropTypes.func,
-  successModalHandler: PropTypes.func,
-  ingredients: PropTypes.object,
-  totalPrice: PropTypes.number
-};
+  shouldComponentUpdate(nextProps) {
+    // Do not render if it's not shown
+    return nextProps.modalShown !== this.props.modalShown;
+  }
+
+  render() {
+    const ingredientsList = Object.keys(this.props.ingredients).map(key => {
+      return (
+        <span key={key}>
+          {key} : {this.props.ingredients[key]}{' '}
+        </span>
+      );
+    });
+
+    return (
+      <OrderModal
+        title={'Finish your order'}
+        visible={this.props.modalShown}
+        onCancel={this.props.closeModalHandler}
+        onSuccess={this.props.successModalHandler}
+        cancelButtonText={'Cancel'}
+        successButtonText={'Order'}
+      >
+        <div>Total price: ${this.props.totalPrice}</div>
+        {ingredientsList}
+      </OrderModal>
+    );
+  }
+}
 
 export default BurgerOrderModal;
